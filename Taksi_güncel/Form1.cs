c4cb4deb193212636ac9e7ded38375b8fde27c0f
Furkan Emir Dursun <furkanemirdursun@gmail.com>
2026-08-06 14:57:08 +0300

    Initial commit

---
+++ Taksi_güncel/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taksi_güncel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        VTI.Veritabani vt=new VTI.Veritabani();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("kullanıcı adı ve Şifre boş bırakılamaz !!!");
+                return;
+            }
+
+            //try
+            //{
+            DataTable dt = vt.Select(@"Select* from tbl_kullanici where Kullanici_Adi= '" + textBox1.Text + "'AND Sifre='" + MD5(textBox2.Text) + "'");
+            if (dt.Rows.Count > 0)
+            {
+                
+                FrmAnaForm fr = new FrmAnaForm();
+                this.Hide();
+                fr.Show();
+
+            }
+        }
+    }
+}
---
+++ Taksi_güncel/Veritabanı.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace VTI
+{
+    class Veritabani
+    {
+        public Veritabani()
+        {
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=.;Initial Catalog=db_otopark;Integrated Security=True");
+        SqlCommand komut = new SqlCommand();
+        SqlDataAdapter adtr = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+
+
+        public DataTable Select(string sorgu)
+        {
+            dt = new DataTable();
+            if (baglanti.State != ConnectionState.Open)
+                baglanti.Open();
+
+            komut.CommandText = sorgu;
+            komut.Connection = baglanti;
+
+            adtr.SelectCommand = komut;
+            adtr.Fill(dt);
+
+            baglanti.Close();
+
+            return dt;
+        }
+
+        public object Insert(string sorgu)
+        {
+            dt = new DataTable();
+            if (baglanti.State != ConnectionState.Open)
+                baglanti.Open();
+            komut.CommandText = sorgu;
+            komut.Connection = baglanti;
+            int kayitSay = komut.ExecuteNonQuery();
+
+            komut.CommandText = "select SCOPE_IDENTITY()";
+            adtr.SelectCommand = komut;
+            adtr.Fill(dt);
+            int kayit_id = 0;
+            if (dt.Rows.Count > 0)
+                kayit_id = Convert.ToInt32(dt.Rows[0][0]);
+            baglanti.Close();
+
+            return kayit_id;
+        }
+
+        public int UpdateDelete(string sorgu)
+        {
+            dt = new DataTable();
+            if (baglanti.State != ConnectionState.Open)
+                baglanti.Open();
+            komut.CommandText = sorgu;
+            komut.Connection = baglanti;
+            int kayitSay = komut.ExecuteNonQuery();
+            baglanti.Close();
+
+            return kayitSay;
+        }
+
+    }
+}
